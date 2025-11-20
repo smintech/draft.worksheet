1e409a70d4233cf9cee8bbe7459ef5a0c1d7617b
Sm in tech <ellaelearnor66@gmail.com>
2025-11-19 19:28:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+const users = [
+    {name: "Alice", age: 20},
+    {name: "Bob", age: 25},
+
 document.getElementById("LoadUser").addEventListener("click", () => {
-    fetch("users.json")
-        .then(response => response.json())
-        .then(data => {
-            const ul = document.getElementById("userlist");
-            ul.innerHTML = "";
-            data.forEach(user => {
-                const li = document.createElement("li");
-                li.innerText = user.name + " - " + user.age;
-                ul.appendChild(li);
-            });
+    const ul = document.getElementById("userlist");
+    ul.innerHTML = "";
+    users.forEach(user => {
+        const li = document.createElement("li");
+        li.innerText = user.name + " - " + user.age;
+        ul.appendChild(li);
+    });
 });
