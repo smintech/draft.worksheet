b294d1de30efa425a8ff4e59fb4f0dbd7bacd77d
Sm in tech <ellaelearnor66@gmail.com>
2025-11-19 20:39:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 document.getElementById("LoadUser").addEventListener("click", () => {
     fetch("https://raw.githubusercontent.com/smintech/draft.worksheet/main/users.json")
-    .then(response => response.json())
-    .then(data => {
-        const ul = document.getElementById("userlist");
-        ul.innerHTML = "";
-        data.forEach(user => {
-            const li = document.createElement("li");
-            li.innerText = user.name + " - " + user.age;
-            ul.appendChild(li);
+        .then(response => response.json())
+        .then(data => {
+            const ul = document.getElementById("userlist");
+            ul.innerHTML = "";
+            
+            data.forEach(user => {
+                const li = document.createElement("li");
+                li.innerText = user.name + " - " + user.age;
+                ul.appendChild(li);
+            });
         });
-    });
 });
